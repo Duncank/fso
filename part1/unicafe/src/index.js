bb009a9ffc4da962116cd5d9236bb47bf13915b9
Duncan Krebbers <duncan@projectfive.nl>
2019-09-05 22:27:19 +0200

    Finish unicafe

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -3,28 +3,54 @@ import ReactDOM from 'react-dom'
 
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 const Title = ({text}) => <h1>{text}</h1>
-const Results = ({results}) => {
-    const resultslist = Object.keys(results).map(it => (<Result name={it} value={results[it]} key={it} />));
+const Statistics = ({results}) => {
+    if (results.total) {
+        const list = Object.keys(results).map(it => (<Statistic text={it} value={results[it]} key={it} />));
 
-    return (
-        <>
-            <Title text = "Results" />
-            {resultslist}
-        </>
-    )
+        return (
+            <>
+                <Title text="Statistics" />
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Value</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {list}
+                    </tbody>
+                </table>
+            </>
+        )
+    } else {
+        return (
+            <>
+                <Title text="Statistics" />
+                No feedback given
+            </>
+        )
+    }
 }
 
-const Result = ({name, value}) => <li>{name}: {value}</li>
+const Statistic = ({text, value}) => <tr><td>{text}</td><td>{value}</td></tr>
 
 const App = () => {
     // save clicks of each button to own state
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
+
+    const totalVotes = good + neutral + bad;
+    const totalPositiveVotes = good + neutral;
+    const totalNegativeVotes = good - bad;
     const results = {
         good,
         neutral,
         bad,
+        total: totalVotes,
+        average: ((totalNegativeVotes || 0) / totalVotes) || 0,
+        positivePercentage: totalVotes ? (((totalPositiveVotes || 0) / totalVotes) * 100).toString() + '%' : 0
     };
 
     return (
@@ -35,7 +61,7 @@ const App = () => {
             <Button onClick={() => setBad(bad + 1)} text="Bad" />
 
             <br />
-            <Results results={results} />
+            <Statistics results={results} />
         </div>
     )
 }
