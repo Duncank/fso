16bf7f7da2cd90e731529699e1297a556bba09b0
Duncan Krebbers <duncan@projectfive.nl>
2019-11-20 20:40:23 +0100

    add build to backend

---
+++ part3/backend/build/precache-manifest.272c38d02cebc3e30b458473e3038393.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e52e8909afe733e05ef65f5bdbd1b982",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec9e7b4b6fb3c0e79013",
+    "url": "/static/css/main.dbc73bf7.chunk.css"
+  },
+  {
+    "revision": "6b1b471eb5fa6b0acc1e",
+    "url": "/static/js/2.82aae917.chunk.js"
+  },
+  {
+    "revision": "ec9e7b4b6fb3c0e79013",
+    "url": "/static/js/main.f7599f15.chunk.js"
+  },
+  {
+    "revision": "9501c9690f81a7e1095c",
+    "url": "/static/js/runtime~main.c186a9c0.js"
+  }
+]);
